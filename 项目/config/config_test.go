c221182cf1245a94226e4fe70ec6eef1199fecb9
yaoyt98 <yaoyt98@163.com>
2021-01-30 22:12:40 +0800

    提交配置库项目

---
+++ 项目/config/config_test.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Config struct {
+	ServerConf ServerConfig `ini:"server"`
+	MysqlConf  MysqlConfig  `ini:"mysql"`
+}
+
+type ServerConfig struct {
+	Ip   string `ini:"ip"`
+	Port uint   `ini:"port"`
+}
+
+type MysqlConfig struct {
+	Username string  `ini:"username"`
+	Passwd   string  `ini:"passwd"`
+	Database string  `ini:"database"`
+	Host     string  `ini:"host"`
+	Port     int     `ini:"port"`
+	Timeout  float32 `ini:"timeout"`
+}
+
+func TestConfig(t *testing.T) {
+	var config1 Config
+	err := ConfigFileSerialization("config.ini", &config1)
+	if err != nil {
+		t.Errorf("反序列配置文件错误，%v", err)
+	}
+	fmt.Printf("打印配置文件结构体：%#v\n", config1)
+}
